d43e6f9898d32070cf052a045a61c8bbaaa5281c
jack <biffisbuffm@gmail.com>
2020-10-17 18:23:32 -0500

    edits'

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -44,11 +44,20 @@ const resolvers = {
     Mutation: {
         addUser: async (parent, args) => {
             const user = await User.create(args);
+            const token = signToken(user);
 
-            return user;
+            return {token, user};
 
         },
-        login: async () => {
+        login: async (parent, {email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+            return { token, user }
 
         }
     }
