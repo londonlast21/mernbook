0bf5d2b4fb67b9120e99c8fc310bfabed644d0a7
jack <biffisbuffm@gmail.com>
2020-10-17 22:10:42 -0500

    restructuring

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -32,26 +32,26 @@ const typeDefs = gql`
         link: String
     },
 
-
-    type Query {
-       me: User
-       users: [User]
-       user(username: String!): User
-       books(username: String!): [Book]
-       book(bookId: String!): Book
-    },
-
-    type Mutation {
-        login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(bookId: String!, authors: [String]!, description: String!, title: String!, image: String!, link: String!): Book
-        removeBook(bookId: String!): User
-        
-    }
-
     type Auth {
         token: ID!
         user: User
+    },
+
+
+    type Query {
+        me: User
+        users: [User]
+        user(username: String!): User
+        books(username: String!): [Book]
+        book(bookId: String!): Book
+     },
+ 
+     type Mutation {
+         login(email: String!, password: String!): Auth
+         addUser(username: String!, email: String!, password: String!): Auth
+         saveBook(bookId: String!, authors: [String]!, description: String!, title: String!, image: String!, link: String!): Book
+         removeBook(bookId: String!): User
+         
     }
 
 `
