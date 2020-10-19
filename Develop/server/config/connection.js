6b740930d8f52132f9b4b352fe25633432b681a5
jack <biffisbuffm@gmail.com>
2020-10-18 23:16:21 -0500

    work

--- Develop/server/config/connection.js
+++ Develop/server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernbook', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
